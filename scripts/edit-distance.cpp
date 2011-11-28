8d870a21cf6676156829b4d5fc3154e1e24fc67f
Stefano Beretta <ste.beretta@gmail.com>
2011-11-28 14:26:23 +0100

    Scripts License

--- scripts/edit-distance.cpp
+++ scripts/edit-distance.cpp
@@ -1,3 +1,28 @@
+/**
+* RNA-seq-Graph
+* Method for reconstructing the Isoform Graph of a gene from RNA-seq data, without the genome information
+*
+* Copyright (C) 2011 Stefano Beretta <ste.beretta(-at-)gmail.com>
+*
+* Distributed under the terms of the GNU Affero General Public License (AGPL)
+*
+*
+* This file is part of RNA-seq-Graph.
+*
+* RNA-seq-Graph is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* RNA-seq-Graph is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* A full copy of the GNU Affero General Public License is reported into
+* the file COPYING. However more informations ca be found at
+* <http://www.gnu.org/licenses/>.
+**/
 /***********************************/
 /* Refine pre-processing with edit */
 /* distance identification among   */
